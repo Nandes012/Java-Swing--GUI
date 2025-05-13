630135316dbdb2da2c2f6582f4a9d577266c407b
Fernandes Howard <fernandeshoward2005@gmail.com>
2025-05-13 12:26:06 +0800

    add image in pop up

--- CharacterCard.java
+++ CharacterCard.java
@@ -61,38 +61,48 @@ public class CharacterCard extends JPanel {
         });
     }
 
-    private void showPopup() {
-        String message;
-        switch (descriptionType) {
-        case "Equipment":
-            message = "Name: " + name + "\nEquipment: " + description;
-            break;
-        case "Race":
-            message = "Name: " + name + "\nRace: " + description;
-            break;
-        case "EP":
-            message = "Name: " + name + "\nEP: " + description;
-            break;
-        case "Skills":
-            message = "Name: " + name + "\nSkills: " + description;
-            break;
-        case "Residence":
-            message = "Name: " + name + "\nResidence: " + description;
-            break;
-        case "Gallery":
-            message = "Name: " + name + "\nGallery: " + description;
-            break;
-        case "Settings":
-            message = "Name: " + name + "\nSettings: " + description;
-            break;
-        default:
-            message = "Name: " + name + "\nCharacter: " + description;
-            break;
+private void showPopup() {
+    // Create a panel for the pop-up content
+    JPanel popupPanel = new JPanel(new BorderLayout(10, 10));
+    popupPanel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+
+    // Add the image to the pop-up
+    JLabel imageLabel = new JLabel();
+    if (filename != null) {
+        try {
+            BufferedImage image = ImageIO.read(new File("Images/" + filename));
+            ImageIcon icon = new ImageIcon(image.getScaledInstance(120, 180, Image.SCALE_SMOOTH));
+            imageLabel.setIcon(icon);
+            imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        } catch (IOException e) {
+            imageLabel.setText("No Image Available");
+            imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
+            imageLabel.setForeground(Color.RED);
+        }
     }
+    popupPanel.add(imageLabel, BorderLayout.WEST);
 
-    JOptionPane.showMessageDialog(this, message, "Details", JOptionPane.INFORMATION_MESSAGE);
+    // Add the description to the pop-up
+    JPanel textPanel = new JPanel(new BorderLayout());
+    textPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+    JLabel nameLabel = new JLabel("Name: " + name);
+    nameLabel.setFont(new Font("Arial", Font.BOLD, 16));
+    textPanel.add(nameLabel, BorderLayout.NORTH);
+
+    JTextArea descriptionArea = new JTextArea(descriptionType + ": " + description);
+    descriptionArea.setEditable(false);
+    descriptionArea.setWrapStyleWord(true);
+    descriptionArea.setLineWrap(true);
+    descriptionArea.setFont(new Font("Arial", Font.PLAIN, 14));
+    descriptionArea.setBackground(popupPanel.getBackground());
+    textPanel.add(descriptionArea, BorderLayout.CENTER);
+
+    popupPanel.add(textPanel, BorderLayout.CENTER);
+
+    // Show the pop-up
+    JOptionPane.showMessageDialog(this, popupPanel, "Character Details", JOptionPane.INFORMATION_MESSAGE);
 }
-
     // ImagePanel class remains the same
     private static class ImagePanel extends JPanel {
         private BufferedImage image;
